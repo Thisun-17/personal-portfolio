e1e1e581c9eb773f114c4e677f52bf3f6fbee770
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-31 00:06:08 +0530

    fix: correct email address format in Email component

--- portfolio/src/components/Email.tsx
+++ portfolio/src/components/Email.tsx
@@ -9,11 +9,11 @@ const Email: React.FC<EmailProps> = ({ showContent }) => {
     <div className={`fixed bottom-0 right-5 xl:right-10 z-10 transition-all duration-1000 delay-700 ${showContent ? 'opacity-100' : 'opacity-0 translate-y-4'}`}>
       <div className="flex flex-col items-center space-y-5 after:content-[''] after:block after:w-px after:h-24 after:mt-5 after:bg-light-slate">
         <a 
-          href="mailto:thisun.20233173.iit.ac.lk"
+          href="mailto:thisun.20233173@iit.ac.lk"
           className="font-mono text-light-slate hover:text-green hover:-translate-y-1 transition-all duration-200 [writing-mode:vertical-rl]"
           aria-label="Email me"
         >
-          thisun.20233173.iit.ac.lk
+          thisun.20233173@iit.ac.lk 
         </a>
       </div>
     </div>
